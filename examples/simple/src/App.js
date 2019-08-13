4f91d19b9f389883cd31ef257cff704433269e5f
Gant Laborde <gantman@gmail.com>
2019-08-13 17:16:08 -0500

    proof of concept working

--- examples/simple/src/App.js
+++ examples/simple/src/App.js
@@ -3,22 +3,49 @@ import * as tf from '@tensorflow/tfjs'
 import './App.css';
 import {DogsNCats} from './dnc'
 
+// const batchSize = 42
+const size = 32
+const zoom = 2
+const gridX = 20
+const gridY = 3
+
+
 function App() {
   const tensorDisplay = useRef(null)
   useEffect(() => {
     const dnc = new DogsNCats()
     dnc.load().then(async () => {
       console.log('Loaded')
-      const [batch] = dnc.devBatch(2)
+
+      const batchSize = gridX * gridY
+      const [batch] = dnc.devBatch(batchSize)
+      tensorDisplay.current.width = size*batchSize*zoom
+      const tdctx = tensorDisplay.current.getContext('2d')
+      // zoom in a little
+      tdctx.scale(zoom,zoom)
+      let xpos = 0
+      let ypos = 0
+
       tf.unstack(batch).forEach(async tensor => {
-        const imageTensor = tensor.div(255).reshape([
-          32,
-          32,
+        const imageTensor = tensor
+          .div(255)
+          .reshape([
+          size,
+          size,
           3
         ])
 
+        // This creation/use must exist inside loop bc
+        // foreEach async would fight over shared resources
+        const canvas = document.createElement('canvas')
+        canvas.width = size
+        canvas.height = size    
+              
         tensor.print()
-        await tf.browser.toPixels(imageTensor, tensorDisplay.current)
+        await tf.browser.toPixels(imageTensor, canvas)  
+        tdctx.drawImage(canvas, xpos * size, ypos * size, size, size)
+        
+        xpos++
         tensor.dispose()
         imageTensor.dispose()
       }) 
@@ -30,6 +57,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <canvas style={styles.canvas} ref={tensorDisplay} />
         <p>
           Demo of dogs-n-cats NPM library
         </p>
@@ -41,7 +69,7 @@ function App() {
         >
           GitHub Link
         </a>
-        <canvas style={styles.canvas} ref={tensorDisplay} />
+        
       </header>
     </div>
   );
@@ -49,7 +77,7 @@ function App() {
 
 const styles = {
   canvas: {
-    maxWidth: '100%',
+    // maxWidth: '100%',
     alignSelf: 'center'
   }
 }
