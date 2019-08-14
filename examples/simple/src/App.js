8cdbf842ebb000c8af6310d1a02a4d710c87b3be
Gant Laborde <gantman@gmail.com>
2019-08-14 16:19:36 -0500

    embed gists

--- examples/simple/src/App.js
+++ examples/simple/src/App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef } from 'react';
-import * as tf from '@tensorflow/tfjs'
+//import * as tf from '@tensorflow/tfjs'
 import './App.css';
+import Gist from 'react-gist'
 import {DogsNCats} from './dnc'
+
 
 const gridX = 20
 const gridY = 3
@@ -38,7 +40,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          GitHub Link
+          GitHub
         </a>
         <p>1,000 32x32 Dogs and 1,000 32x32 Cats from the <a 
           href="https://www.cs.toronto.edu/~kriz/cifar.html"      
@@ -48,9 +50,9 @@ function App() {
         <canvas style={styles.canvas} ref={tensorCatDisplay} />
       </header>
       <div>
-        <img src="dnc_logo.png" class="logo" />
+        <img src="dnc_logo.png" className="logo" />
         <h2>Example Usage</h2>
-        <script src="https://gist.github.com/GantMan/78189f19457449a09ecc05eb927f686c.js"></script>
+        <Gist id='78189f19457449a09ecc05eb927f686c' file='dog-cat.js' />
       </div>
     </div>
   );
